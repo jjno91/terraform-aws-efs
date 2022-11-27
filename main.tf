03a50d90737872d0fd9746008f018c83d03fd9b6
Jeremiah Norris <jjno91@gmail.com>
2022-11-27 11:05:15 -0600

    replace backup resources

--- main.tf
+++ main.tf
@@ -5,38 +5,10 @@ resource "aws_efs_file_system" "this" {
   tags                            = { Name = var.name }
 }
 
-resource "aws_backup_vault" "this" {
-  name = var.name
-}
+resource "aws_efs_backup_policy" "this" {
+  file_system_id = aws_efs_file_system.this.id
 
-resource "aws_backup_plan" "this" {
-  name = var.name
-
-  rule {
-    rule_name         = "main"
-    target_vault_name = aws_backup_vault.this.name
-    schedule          = var.schedule
-
-    lifecycle {
-      cold_storage_after = var.cold_storage_after
-      delete_after       = var.delete_after
-    }
+  backup_policy {
+    status = "ENABLED"
   }
-}
-
-resource "aws_backup_selection" "this" {
-  name         = "main"
-  iam_role_arn = aws_iam_role.this.arn
-  plan_id      = aws_backup_plan.this.id
-  resources    = [aws_efs_file_system.this.arn]
-}
-
-resource "aws_iam_role" "this" {
-  name_prefix        = "${var.name}-efs-backup-"
-  assume_role_policy = data.aws_iam_policy_document.this.json
-}
-
-resource "aws_iam_role_policy_attachment" "this" {
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSBackupServiceRolePolicyForBackup"
-  role       = aws_iam_role.this.name
 }
